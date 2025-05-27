148e4dc0b64eb18134f471a4b09091008bf40126
sonu kushwaha <sonukuswhaha@gmail.com>
2025-05-28 02:28:13 +0530

    Add files via upload

---
+++ type_casting.java
@@ -0,0 +1,16 @@
+public class myfirst{
+    publi static void main(string[] args){
+        // 
+        
+        // int x=10;
+        // double y =x;
+
+
+         double x=10;
+        int y =x;
+        System.out.println("the value of x is");
+       System.out.println( x);
+        System.out.println("the value of y is");
+         System.out.println( y);
+    }
+}
