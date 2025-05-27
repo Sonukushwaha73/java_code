b5e4585850d9443fc16e275cff04818928e79924
sonu kushwaha <sonukuswhaha@gmail.com>
2025-05-28 02:28:58 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,31 @@
+  public class second {
+    public static void main(String[] args) {
+        //Arthmetic operatorss
+        int a = 15;
+        int b = 18;
+       
+        int c = a + b; // yeh addition hai
+        System.out.println(c);
+
+        c = a - b; // yeh subtraction hai
+        System.out.println(c);
+
+
+
+
+
+
+ int a = 15;
+        int b = 18;
+       
+        int c = a *b; // yeh addition hai
+        System.out.println(c);
+
+        c = a / b; // yeh subtraction hai
+        System.out.println(c);
+
+
+
+    }
+}
+
